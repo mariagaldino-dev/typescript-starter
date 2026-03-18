676c640e64d56dbb6280c58628eb38c67d024544
mariagaldino-dev <maria.galdino@estudante.ifms.edu.br>
2026-03-18 12:19:45 -0400

    18/03/26

---
+++ src/repositorio/index.ts
@@ -0,0 +1,5 @@
+export * from './create-todo.repositorio';
+export * from './delete-todo.repositorio';
+export * from './find-all-todo.repositorio';
+export * from './find-todo-by-id.repositorio';
+export * from './update-todo.repositorio';
---
+++ src/todos/use-cases/create-todo.use-case.ts
@@ -0,0 +1,23 @@
+import { Injectable, Logger } from "@nestjs/common";
+import { CreateTodoRepository } from "src/repositorio";
+import { CreateTodoDto } from "../dto/create-todo.dto";
+
+@Injectable()
+export class CreateTodoUseCase {
+    constructor(
+        private readonly createTodoRepositorio: CreateTodoRepository,
+        private readonly logger: Logger,
+    ) { }
+
+    async execute(data: CreateTodoDto) {
+        try {
+            this.logger.log('Creating toDo...');
+            const todo = await this.createTodoRepositorio.create(data);
+            this.logger.log('ToDo created successfully');
+            return todo;
+        } catch (error) {
+            this.logger.error(error);
+            throw new Error('Failed to create toDo');
+        }
+    }
+}
