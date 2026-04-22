3c84323f3e06ce63322b000f48ad53dfae945ac5
mariagaldino-dev <maria.galdino@estudante.ifms.edu.br>
2026-04-22 12:35:30 -0400

    ssss

---
+++ src/shared/database/prisma.database.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '@prisma/client/scripts/default-index.js';
+
+@Injectable()
+export class PrismaService extends PrismaClient {
+  constructor() {
+    const adapter = new PrismaPg({
+      connectionString: process.env.DATABASE_URL as string,
+    });
+    // Prisma types are generated at runtime; eslint cannot fully resolve this call.
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+    super({ adapter });
+  }
+}
